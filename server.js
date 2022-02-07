736924d041a5d349984739d2a03c474aaa795e6f
LewZhiYang <circle1329@gmail.com>
2022-02-07 15:13:39 +0800

    added comments for understanding

--- server.js
+++ server.js
@@ -8,14 +8,19 @@ require('dotenv').config({path: './config/.env'})
 
 connectDB()
 
+// defining express settings
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+// when main page is loaded use this
 app.use('/', homeRoutes)
+
+// when subdirectory starts with todos
 app.use('/todos', todoRoutes)
- 
+
+// starts server
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is running at ${process.env.PORT}, you better catch it!`)
 })    
